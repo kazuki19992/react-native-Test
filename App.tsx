25a6c24685f6dd719161b391070cee58b933aca0
kazuki19992 <kushida98@gmail.com>
2022-05-14 23:07:35 +0900

    initialized

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textTitle}>Hello World!</Text>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textTitle: {
+    color: "#ffaa00",
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+  text: {
+    color: "white"
+  }
+});
